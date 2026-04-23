b09495506ccda67c595c2f758b7aca8b666b96fc
Hope <ana.s.morais@aluno.senai.br>
2026-04-23 16:57:22 -0300

    primeiro commit

---
+++ Introdução/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Primeiro PHP</title>
+</head>
+<body>
+    <h1>PHP abaixo:</h1>
+    <?php
+        echo "Olá, visitante!";
+        echo "<p> Isso é um paragrafo</p>";
+        echo "<strong> Isso é negrito</strong>";
+        # Isso é um comentário
+        /* Isso é um comentário*/
+        // Isso é um comentário
+    ?>
+</body>
+</html>
+
