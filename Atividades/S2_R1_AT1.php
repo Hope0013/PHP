5ecc155f78e84dfd589f99c98e1ecc60ced58297
Hope <ana.s.morais@aluno.senai.br>
2026-04-23 17:11:49 -0300

    nova atividade

---
+++ Atividades/S2_R1_AT1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa XYZ</title>
+</head>
+<body>
+    <h1><strong>Empresa XYZ</strong></h1>
+    <!-- Colocamos o php dentro do body -->
+    <?php
+        echo"<p>Informações da Empresa...</p>";
+        // Em php, sempre colocar ";" no final da linha
+        echo"<p>Para compras, ligue: (19) 99999-8888</p>";
+        echo"<p>Contate a empresa para mais dúvidas!</p>";
+
+        // Para rodar o php colocamos no terminal: php -S localhost:(porta)
+    ?>
+</body>
+</html>
