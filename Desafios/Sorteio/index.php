747f110b87ca6e1c4df71199ba83707ff31bdad5
Hope <ana.s.morais@aluno.senai.br>
2026-05-14 13:22:42 -0300

    desafio 2

--- Desafios/Sorteio/index.php
+++ Desafios/Sorteio/index.php
@@ -4,20 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sorteio</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>SORTEIO DA MEGA-SENA</h1>
     <?php
-        function $sortear(){
-            $min = 1;
-            $max = 60;
-            $qtd = 6;
+    // fazendo uma funçaõ para sortear os numeros
+        function sortear($min, $max, $qtd){
 
-            $nume
+        // os numeros vao ser sorteados entre o minimo e o maximo
+            $numeros = range($min, $max);
+            // o shuffle é responsavel por pegar os numeros sorteados e 
+            // embaralha-los para serem armazenados no array
+            shuffle($numeros);
+
+            // o array_lince guarda os numeros sorteados
+            return array_slice($numeros, 0, $qtd);
+        }
+
+        // se o botao for acionado
+        if (isset($_GET['btn-sortear'])){
+            // a variavel resultado vai guardar os 6 numeros sorteados de 1 a 60 
+            $resultado = sortear(1,60, 6);
+
+            // coloca cada numero num span para podermos fazer um css mais bonitinho
+            // cada numero vai estar numa caixinha
+            echo "<div class='container-numeros'>";
+                foreach($resultado as $num){
+                    echo "<span class = 'caixinha'>$num</span>";
+                }
+            echo "</div>";
         }
     ?>
-
-    <form action="$sortear">
-        <button type="submit">SORTEAR</button>
+    <!-- botao que vai executar a funcao sortear -->
+    <form method="GET">
+        <button class="button" type="submit" name="btn-sortear">SORTEAR</button>
     </form>
 </body>
 </html>
